d0503ed9199d0f03d043f6af97982645fa79a84c
tong200342 <57955847+tong200342@users.noreply.github.com>
2020-02-26 15:04:56 +0700

    Add files via upload

---
+++ load.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * load.php.
+ *
+ * @author Goragod Wiriya <admin@goragod.com>
+ * @copyright 2016 Goragod.com
+ * @license http://www.kotchasan.com/license/
+ *
+ * @see http://www.kotchasan.com/
+ */
+/*
+ * Site root
+ */
+define('ROOT_PATH', str_replace('\\', '/', dirname(__FILE__)).'/');
+/*
+ * โฟลเดอร์เก็บข้อมูล
+ */
+define('DATA_FOLDER', 'datas/');
+/*
+ * 0 (default) บันทึกเฉพาะข้อผิดพลาดร้ายแรงลง error_log .php
+ * 1 บันทึกข้อผิดพลาดและคำเตือนลง error_log .php
+ * 2 แสดงผลข้อผิดพลาดและคำเตือนออกทางหน้าจอ (ใช้เฉพาะตอนออกแบบเท่านั้น)
+ */
+define('DEBUG', 0);
+/*
+ * false (default)
+ * true บันทึกการ query ฐานข้อมูลลง log (ใช้เฉพาะตอนออกแบบเท่านั้น)
+ */
+define('DB_LOG', false);
+/*
+ * ภาษาอัตโนมัติจากบราวเซอร์
+ */
+define('AUTO_LANGUAGE', false);
+/*
+ * ระบุ SQL Mode ที่ต้องการ
+ * หากพบปัญหาการใช้งาน
+ */
+//define('SQL_MODE', '');
+/*
+ * รายรับ
+ */
+define('RECEIVE', 1);
+/*
+ * รายจ่าย
+ */
+define('EXPENSE', 2);
+/*
+ * กระเป๋าเงิน
+ */
+define('WALLET', 4);
+/**
+ * load Kotchasan.
+ */
+include 'Kotchasan/load.php';
